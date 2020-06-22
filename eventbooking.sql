d7d154a235589d40fb3fefdf98f5165fd628475c
Olek <an00010@gmail.com>
2020-06-22 21:13:13 +0200

    db update

--- eventbooking.sql
+++ eventbooking.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 22 Cze 2020, 18:21
+-- Czas generowania: 22 Cze 2020, 21:12
 -- Wersja serwera: 10.4.11-MariaDB
 -- Wersja PHP: 7.4.3
 
@@ -33,37 +33,8 @@ CREATE TABLE `event` (
   `name` varchar(100) NOT NULL,
   `description` varchar(500) NOT NULL,
   `location` varchar(70) NOT NULL,
-  `numberOfTickets` int(10) UNSIGNED NOT NULL,
+  `numberOfReservations` int(10) UNSIGNED NOT NULL,
   `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `ticket`
---
-
-CREATE TABLE `ticket` (
-  `id` varchar(255) NOT NULL,
-  `firstName` varchar(30) NOT NULL,
-  `lastName` varchar(40) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phoneNumber` varchar(9) NOT NULL,
-  `ticketTypeId` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `tickettype`
---
-
-CREATE TABLE `tickettype` (
-  `id` varchar(255) NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `price` int(11) UNSIGNED NOT NULL,
-  `numberAvailable` int(11) UNSIGNED NOT NULL,
-  `eventId` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -75,36 +46,6 @@ CREATE TABLE `tickettype` (
 --
 ALTER TABLE `event`
   ADD PRIMARY KEY (`id`);
-
---
--- Indeksy dla tabeli `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ticketTypeId` (`ticketTypeId`);
-
---
--- Indeksy dla tabeli `tickettype`
---
-ALTER TABLE `tickettype`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `eventId` (`eventId`);
-
---
--- Ograniczenia dla zrzutów tabel
---
-
---
--- Ograniczenia dla tabeli `ticket`
---
-ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_1` FOREIGN KEY (`ticketTypeId`) REFERENCES `tickettype` (`id`);
-
---
--- Ograniczenia dla tabeli `tickettype`
---
-ALTER TABLE `tickettype`
-  ADD CONSTRAINT `tickettype_ibfk_1` FOREIGN KEY (`eventId`) REFERENCES `event` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
